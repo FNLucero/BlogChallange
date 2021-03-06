256e271a4d65f244900f32f597ea8b895a86be3c
FNLucero <facundo.lucero@outlook.es>
2021-03-06 16:09:28 -0300

    Creacion del modelo

---
+++ BlogChallange/Models/Blog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallangeBlog.Models
+{
+    public class Blog
+    {
+        public List<Post> posts = new List<Post>();
+
+        public void AgregarPost(Post post)
+        {
+            posts.Add(post);
+        }
+        public void ModificarPost(int id,Post post)
+        {
+            this.BorrarPost(posts.Find(x => x.Id == id));
+            posts.Add(post);
+        }
+        public void BorrarPost(Post post)
+        {
+            posts.Remove(post);
+        }
+        public Post BuscarPorId(int id)
+        {
+            return posts.Find(x=>x.Id==id);
+        }
+        public List<Post> retornarLista()
+        {
+             return (List<Post>)(posts.OrderByDescending(x => x.FechaDeCreacion));
+        }
+
+    }
+}
---
+++ BlogChallange/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallangeBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public string Categoria { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+
+
+    }
+}
